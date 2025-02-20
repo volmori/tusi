619e5288742228929532771f2b1544bc6ef9f9c9
dos <114744003+volmori@users.noreply.github.com>
2025-02-20 11:46:09 +0800

    script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Tusi please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
